abfb4e52186bb491ac16076e973d50a67c128247
Max Souza <msb@icomp.ufam.edu.br>
2022-11-05 11:27:05 -0400

    Alteracoes no .mk

--- devHome/SmartDoor/smartdoor_devhome.mk
+++ devHome/SmartDoor/smartdoor_devhome.mk
@@ -29,30 +29,25 @@ PRODUCT_PACKAGES += \
     hello_c \
     nano \
     sl \
-    hello_cpp \
-    hello_daemon_cpp \
-    HelloApp \
-    HelloJava \
-    smartlamp_client \
     smartdoor_client
 
 
 BOARD_SEPOLICY_DIRS += device/devHome/SmartDoor/sepolicy
 
-# Smartlamp AIDL Interface
-PRODUCT_PACKAGES += devtitans.smartlamp
+# Smartdoor AIDL Interface
+PRODUCT_PACKAGES += devHome.smartdoor
 
-# Smartlamp Binder Service
-PRODUCT_PACKAGES += devtitans.smartlamp-service
+# Smartdoor Binder Service
+PRODUCT_PACKAGES += devHome.smartdoor-service
 
 # Device Framework Matrix (Declara que o nosso produto Kraken precisa do serviço smartlamp)
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/devtitans/kraken/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/devHome/SmartDoor/device_framework_matrix.xml
 
 # Manager
-PRODUCT_PACKAGES += devtitans.smartlampmanager
+PRODUCT_PACKAGES += devHome.smartdoormanager
 
-# Cliente de Linha de Comando para o Serviço Smartlamp
-PRODUCT_PACKAGES += smartlamp_service_client
+# Cliente de Linha de Comando para o Serviço Smartdoor
+PRODUCT_PACKAGES += smartdoor_service_client
 
-# App Privilegiado de Teste do Serviço Smartlamp
-PRODUCT_PACKAGES += SmartlampTestApp
+# App Privilegiado de Teste do Serviço Smartdoor
+PRODUCT_PACKAGES += SmartdoorTestApp
