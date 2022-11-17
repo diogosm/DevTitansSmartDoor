c5a57606fb1de64028459079f26fd1407586b516
Claver <edward.claver@icomp.ufam.edu.br>
2022-11-17 19:58:12 -0400

    adicionando monitor serial println

--- MCU/devhome_smartdoor/devhome_smartdoor.ino
+++ MCU/devhome_smartdoor/devhome_smartdoor.ino
@@ -2,6 +2,8 @@
 
 // código na função setup é executado apenas uma vez quando o ESP32 ou ARDUINO é iniciado
 void setup() {
+  Serial.begin(9600);
+  
   // inicializar o PIN DIGITAL como uma saída.
   pinMode(RELE, OUTPUT);
 }
@@ -9,7 +11,9 @@ void setup() {
 // o código na função de loop é executado repetidamente infinitamente
 void loop() {
   digitalWrite(RELE, LOW); // destranca a porta
+  Serial.println("PORTA ABERTA");
   delay(5000);
   digitalWrite(RELE, HIGH);  // tranca a porta
+  Serial.println("PORTA FECHADA");
   delay(5000);
 }
